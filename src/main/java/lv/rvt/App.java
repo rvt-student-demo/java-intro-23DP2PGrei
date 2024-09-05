f5235db932ad117d1966b763e364152d51ffa7b4
23DP2PGrei <166109741+23DP2PGrei@users.noreply.github.com>
2024-09-05 09:42:34 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,15 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+
+        Scanner scanner = new Scanner(System.in);
+
+        
 
     }
 
