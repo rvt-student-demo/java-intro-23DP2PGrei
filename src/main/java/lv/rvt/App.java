86725d206f58f455ba3a1879500e06b93f6b7ce9
23DP2PGrei <166109741+23DP2PGrei@users.noreply.github.com>
2024-10-09 09:17:32 +0000

    Star sign (Part 1)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,13 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main(String[] args)     
-    {
-        printUntilNumber(2, 10);
-    
+    public static void main(String[] args) {
+        Stars.printStars(5);
+        Stars.printStars(3);
+        Stars.printStars(9);
     }
-    public static void printUntilNumber(int first, int second) {
-        while(first != second){
-            if (first%3 == 0)
-                System.out.println(first);
-            first++;
-        }
-        }
 
 }
+
+
 
---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,12 @@
+package lv.rvt;
+
+import java.util.*;
+
+public class Stars {
+    public static void printStars(int stars) {
+        for(int i = 0; i < stars; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
