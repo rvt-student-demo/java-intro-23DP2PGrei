c8e27c2e54cc495c1a87c5f24154ffaba7101ec1
23DP2PGrei <166109741+23DP2PGrei@users.noreply.github.com>
2024-10-09 09:24:30 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,6 +10,26 @@ public class App
         Stars.printStars(5);
         Stars.printStars(3);
         Stars.printStars(9);
+
+        System.out.println();
+
+        Stars.printStars(4);
+        Stars.printStars(4);
+        Stars.printStars(4);
+        Stars.printStars(4);
+
+        System.out.println();
+
+        Stars.printStars(17);
+        Stars.printStars(17);
+        Stars.printStars(17);
+        
+        System.out.println();
+
+        Stars.printStars(1);
+        Stars.printStars(2);
+        Stars.printStars(3);
+        Stars.printStars(4);
     }
 
 }
