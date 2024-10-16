52b6c1e0a28dcb5b3d4f993efdeddf241e9a1d64
23DP2PGrei <166109741+23DP2PGrei@users.noreply.github.com>
2024-10-16 08:09:01 +0000

    Third element

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,29 +2,28 @@ package lv.rvt;
 
 import java.util.*;
 
-import java.util.Scanner;
-
-public class App 
-{
+public class App {
     public static void main(String[] args) {
-        Stars.printStars(5);
-        Stars.printStars(3);
-        Stars.printStars(9);
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> wordList = new ArrayList<>();
 
         System.out.println();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            wordList.add(input);
+        }
 
-        Stars.printSquare(4);
+        if (wordList.size() > 2) {
+            System.out.println(wordList.get(2));
+        } else {
+            System.out.println();
+        }
 
-        System.out.println();
-
-        Stars.printRect(17, 3);
-
-        System.out.println();
-
-        Stars.printTriangle(0);
-        Stars.printTriangle(4);
+        scanner.close();
     }
-
 }
 
 
