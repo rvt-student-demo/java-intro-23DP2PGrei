760e6c39d88749e3a05dd00489a7a4266b8a9d4f
23DP2PGrei <166109741+23DP2PGrei@users.noreply.github.com>
2024-10-17 09:14:30 +0000

    Exercise 2 — Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,27 +4,30 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> wordList = new ArrayList<>();
 
-        System.out.println();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.isEmpty()) {
+        Random number = new Random();
+        int randnum = number.nextInt(10);
+
+        System.out.println("I am thinking of a number from 1 to 10.");
+        System.out.println("You must guess what it is in three tries.");
+        System.out.println("Enter a guess: ");
+
+        while(true){
+            int guess = Integer.valueOf(scanner.nextLine());
+            if (guess == randnum){
+                System.out.println("RIGHT!");
+                System.out.println("You have won the game.");
                 break;
+        }
+            else {
+                System.out.println("Wrong!");
             }
-            wordList.add(input);
         }
-
-        if (wordList.size() > 2) {
-            System.out.println(wordList.get(2));
-        } else {
-            System.out.println();
-        }
-
-        scanner.close();
     }
-}
+}  
+
 
 
 
